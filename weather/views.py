cd8399dcdcc48e1a4cb8185d7c7857fe52bcfaa4
Xerdis1337 <75767447+Xerdis1337@users.noreply.github.com>
2025-03-28 15:54:32 +0300

    Update views.py

--- weather/views.py
+++ weather/views.py
@@ -2,7 +2,7 @@ import requests
 from django.http import JsonResponse
 from django.shortcuts import render
 
-API_KEY = '04e7ba7baa7955962bd50aad12bd4aff'
+API_KEY = '3dcd961bb75ce366c84e525929292897'
 
 def get_weather(request):
     city = request.GET.get('city', 'Moscow')
